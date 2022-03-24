c7be0052aacbb3c599687f710a4599efc2153e8f
maxsobolev97 <77441161+maxsobolev97@users.noreply.github.com>
2022-03-24 13:33:08 +0300

    Add files via upload

---
+++ Alerting.ps1
@@ -0,0 +1,13 @@
+﻿$pass = "C:\id-sys\share\answer"
+
+$answer = Get-ChildItem $pass
+
+if($answer -ne $null){
+
+    foreach($error_file in $answer){
+
+        Send-MailMessage -From 'example@example.ru' -To 'example@example.ru' -Subject 'Возникла ошибка' -Body "Необходимо перейти на станцию example!" -Attachments $error_file -Priority High -SmtpServer 'example'
+
+    }
+
+}
